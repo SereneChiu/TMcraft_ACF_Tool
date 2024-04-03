dcd232e583c5e9e8af3f45457979c7f18b13b452
Serene Chiu <serenechiu827@gmail.com>
2024-04-03 15:35:51 +0800

    Add description

--- Ferrobotics_Controller/SetDataModel.cs
+++ Ferrobotics_Controller/SetDataModel.cs
@@ -16,6 +16,16 @@ namespace Ferrobotics_Controller
         private decimal mSetParam4 = 0;
         private string mWriteData = "";
         private bool mTargetDo = false;
+        private string mTitleDesc = "Operation Parameters Information:";
+        private string mTargetDesc  = "f__target:  A positive target force means the ACF will be pushing, while a negative target force means pulling.";
+        private string mZeroDesc    = "f__zero:   This value is necessary for the Force ramp, for details see the chapter Force ramp functionality.";
+        private string mRampDesc    = "t__ramp:  This value is necessary for the Force ramp, for details see the chapter Force ramp functionality.";
+        private string mPayloadDesc = "Payload: Determine the weight of the tool, this value is the payload.";
+        private string mSpeedDesc   = "Speed:   You enter the speed in 10rpm increments of the maximum speed (10 000 rpm).\r\nHowever, note the maximum permissible speed of the abrasive!";
+        private string mDetailDesc = "Force ramp functionality: \r\nThe force ramp allows making contact with a surface smoothly in order to avoid abrupt force onto the workpiece. " +
+                                 "\r\nSet a zero force (set_f_zero) that will be applied as long as the ACF does not detect contact." +
+                                 "\r\nSet the time of the force ramp with set_t_ramp. Deactivate the force ramp functionality by setting set_t_ramp to 0.";
+
         public bool Edit_Mode { get { return mEdit_Mode; } set { mEdit_Mode = value; NotifyPropertyChanged("Edit_Mode"); } } 
         public decimal SetParam1 { get { return mSetParam1; } set { mSetParam1 = value; NotifyPropertyChanged("SetParam1"); } }
         public decimal SetParam2 { get { return mSetParam2; } set { mSetParam2 = value; NotifyPropertyChanged("SetParam2"); } }
@@ -23,6 +33,15 @@ namespace Ferrobotics_Controller
         public decimal SetParam4 { get { return mSetParam4; } set { mSetParam4 = value; NotifyPropertyChanged("SetParam4"); } }
         public string WriteData { get { return mWriteData; } set { mWriteData = value; NotifyPropertyChanged("WriteData"); } }
         public bool TargetDo { get { return mTargetDo; } set { mTargetDo = value; NotifyPropertyChanged("TargetDo"); } }
+      
+        public string TitleDesc { get { return mTitleDesc; } set { mTitleDesc = value; NotifyPropertyChanged("TitleDesc"); } }
+        public string TargetDesc { get { return mTargetDesc; } set { mTargetDesc = value; NotifyPropertyChanged("TargetDesc"); } }
+        public string ZeroDesc { get { return mZeroDesc; } set { mZeroDesc = value; NotifyPropertyChanged("ZeroDesc"); } }
+        public string RampDesc { get { return mRampDesc; } set { mRampDesc = value; NotifyPropertyChanged("RampDesc"); } }
+        public string PayloadDesc { get { return mPayloadDesc; } set { mPayloadDesc = value; NotifyPropertyChanged("PayloadDesc"); } }
+        public string SpeedDesc { get { return mSpeedDesc; } set { mSpeedDesc = value; NotifyPropertyChanged("SpeedDesc"); } }
+        public string DetailDesc { get { return mDetailDesc; } set { mDetailDesc = value; NotifyPropertyChanged("DetailDesc"); } }
+
 
         public void MergeStringData()
         {
